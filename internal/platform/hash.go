93218eb51279cadf5f4d48cd4b348e0a340c2207
LuckyLL17 <xiangyujia50@gmail.com>
2026-08-27 12:45:46 +0800

    red：含 Bug 加受保护测试验收快照

---
+++ internal/platform/hash.go
@@ -0,0 +1,19 @@
+package platform
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"strings"
+)
+
+func Hash(value string) string {
+	if value == "" {
+		return ""
+	}
+	sum := sha256.Sum256([]byte(value))
+	return hex.EncodeToString(sum[:])
+}
+
+func ChainHash(previous, value string) string {
+	return Hash(strings.Join([]string{previous, value}, "|"))
+}
